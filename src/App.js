13a3be8211df13aac2768f222c592be565efee28
sally al sardouk <sally.sardouk98@gmail.com>
2020-12-30 08:52:43 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import CatComponent from './CatComponent.js'
+import {GraceHopperQuoteComponent} from'./GraceHopperQuoteComponent.js'
+import MouseComponent from './MouseComponent.js'
 
 class App extends Component {
 	render() {
@@ -8,6 +11,7 @@ class App extends Component {
 				<CatComponent />
 				<GraceHopperQuoteComponent />
 				{/* one more component missing */}
+				<MouseComponent/>
 			</div>
 		);
 	}
